be5e6b88c8990b0e46a98d53fd5ccfda731f09ce
Higor Henrique Freitas Rodrigues <freitashigor@hotmail.com>
2022-09-28 23:17:46 -0300

    update config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import { resolve } from "path"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [react()],
+
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                nested: resolve(__dirname, "nested/index.html"),
+            },
+        },
+    },
 })
