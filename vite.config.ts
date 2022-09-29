03a980e99b19679af5ed48030da6e54914554d81
Higor Henrique Freitas Rodrigues <freitashigor@hotmail.com>
2022-09-28 23:26:18 -0300

    test config vite

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                nested: resolve(__dirname, "nested/index.html"),
+                nested: resolve(__dirname, "nested/skills.html"),
             },
         },
     },
